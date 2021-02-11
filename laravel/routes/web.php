8051f0794420788856e80457b347cd46a21edb92
jumpei arai <jumpei0924@gmail.com>
2021-02-11 22:11:24 +0900

    update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,4 @@
 Auth::routes(); //-- この行を追加
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles','ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles','ArticleController')->only(['show']);
